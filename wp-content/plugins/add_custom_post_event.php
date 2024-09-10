6d0028cd3be8c5752dac3606b04c1ac840a06d72
Le Quy Thien <zacubj2@gmail.com>
2024-09-10 08:11:11 +0700

    first commit

---
+++ wp-content/plugins/add_custom_post_event.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: add custom post type event
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Author: lequythien
+Version: 1
+*/
+
+function university_post_types() {
+    register_post_type('event', array(
+      'show_in_rest' => true,
+      'supports' => array('title', 'editor', 'excerpt'),
+      'rewrite' => array('slug' => 'events'),
+      'has_archive' => true,
+      'public' => true,
+      'labels' => array(
+        'name' => 'Events',
+        'add_new_item' => 'Add New Event',
+        'edit_item' => 'Edit Event',
+        'all_items' => 'All Events',
+        'singular_name' => 'Event'
+      ),
+      'menu_icon' => 'dashicons-calendar'
+    ));
+  }
+  
+  add_action('init', 'university_post_types');
---
+++ wp-content/themes/university/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+<header class="site-header">
+      <div class="container">
+        <h1 class="school-logo-text float-left">
+        <?php
+            $sitename = get_bloginfo('title'); // Lấy tên của trang web
+            $parts = explode(' ', $sitename); // Tách tên thành các phần dựa trên dấu cách
+            if (count($parts) >= 2) {
+                $first_part = $parts[0];
+                echo '<strong><a href="' . get_bloginfo('home') . '">' . $first_part . '</strong></a>';
+                
+                $remaining_parts = array_slice($parts, 1);
+                if (!empty($remaining_parts)) {
+                    echo ' <span style="color: white;">' . implode(' ', $remaining_parts) . '</span>';
+                }
+            } else {
+                echo '<strong><a href="' . get_bloginfo('home') . '">' . $sitename . '</strong></a>';
+            }
+          ?>
+
+        </a>
+        </h1>
+        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+        <div class="site-header__menu group">
+          <nav class="main-navigation">
+            <ul>
+              <?php 
+                wp_nav_menu( array(
+                    'theme_location' => 'primary_menu',
+                    'container'            => 'nav',
+                    'container_class'      => 'main-navigation',
+                  ) );
+              ?>
+            </ul>
+          </nav>
+
+          <div class="site-header__util">
+            <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+            <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+          </div>
+        </div>
+      </div>
+    </header>
